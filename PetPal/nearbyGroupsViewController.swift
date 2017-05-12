5b77ea773849426e2bece3376e648195bbe6e858
Rui Mao <ruimao18@gmail.com>
2017-05-11 17:51:27 -0700

    add segue from nearbyGroup to detailGroup

--- PetPal/nearbyGroupsViewController.swift
+++ PetPal/nearbyGroupsViewController.swift
@@ -161,18 +161,17 @@ class NearbyGroupsViewController: UIViewController,  MKMapViewDelegate, CLLocati
         return annotationView
         }
     }
-    
-    
-    
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "groupDetailSegue" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let detailGroupVC = segue.destination as! GroupDetailViewController
+                detailGroupVC.group = groups[indexPath.row]
+            }
+        }
     }
-    */
+
 
 }
